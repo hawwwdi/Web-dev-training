7f73cb96e3b6ca2bb31cb80419c6dccfaf789dac
hadi <hawwwdi@gmail.com>
2020-09-03 15:15:18 +0430

    fix minor bug

--- simple-blog/db.go
+++ simple-blog/db.go
@@ -4,17 +4,18 @@ import (
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
 	"os"
+	"time"
 )
 
 var db *sql.DB
 
 func init() {
 	var err error
-	db, err = sql.Open("sql", "hadi:123%Ha%123@localhost/blog")
+	db, err = sql.Open("sql", "hadi:123%Ha%123@tcp(127.0.0.1)/blog")
 	handleErr(os.Stderr, err)
 	err = db.Ping()
 	handleErr(os.Stderr, err)
+	db.SetConnMaxLifetime(time.Minute * 3)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
 }
-
-
-
