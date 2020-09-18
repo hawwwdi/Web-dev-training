e6e7b3948fc2ee9f9cd067b318b169fb59d8abb4
hadi <hawwwdi@gmail.com>
2020-09-18 19:01:52 +0430

    client side completed

--- jwt/client.go
+++ jwt/client.go
@@ -1,9 +1,39 @@
 package main
 
 import (
+	"fmt"
 	"github.com/dgrijalva/jwt-go"
+	"io/ioutil"
+	"net/http"
+	"sync"
 	"time"
 )
+
+type httpClient struct {
+	client http.Client
+	sync.Mutex
+}
+
+var client = &httpClient{}
+
+func makeReq(w http.ResponseWriter, r *http.Request) {
+	jwtToken, err := generateJWT()
+	if err != nil {
+		fmt.Fprint(w, err.Error())
+		return
+	}
+	req, _ := http.NewRequest("GET", address, nil)
+	req.Header.Set("Token", jwtToken)
+	client.Lock()
+	res, err1 := client.client.Do(req)
+	client.Unlock()
+	if err1 != nil {
+		fmt.Fprint(w, err1.Error())
+		return
+	}
+	body, _ := ioutil.ReadAll(res.Body)
+	fmt.Fprint(w, string(body))
+}
 
 func generateJWT() (string, error) {
 	token := jwt.New(jwt.SigningMethodHS256)
@@ -17,5 +47,6 @@ func generateJWT() (string, error) {
 	if err != nil {
 		return "", err
 	}
+	fmt.Printf("token is: %v", tokenString)
 	return tokenString, nil
 }
--- jwt/main.go
+++ jwt/main.go
@@ -5,11 +5,13 @@ import (
 	"net/http"
 )
 
+const address = "localhost:8080"
+
 var key = []byte("123456")
 
 func main() {
 	http.Handle("/hello", isAuthorized(helloWorld))
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(address, nil)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
