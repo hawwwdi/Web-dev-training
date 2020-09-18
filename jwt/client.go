b719334f5bb2ca03c7a409cc583007423c7d5df6
hadi <hawwwdi@gmail.com>
2020-09-18 18:30:50 +0430

    develope generateJWT function

--- jwt/client.go
+++ jwt/client.go
@@ -1,2 +1,21 @@
 package main
 
+import (
+	"github.com/dgrijalva/jwt-go"
+	"time"
+)
+
+func generateJWT() (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
+	claims := token.Claims.(jwt.MapClaims)
+
+	claims["authorized"] = true
+	claims["client"] = "hawwwdi"
+	claims["exp"] = time.Now().Add(time.Minute * 30).Unix()
+
+	tokenString, err := token.SignedString(key)
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
